1e3cbf5d3b9117cf4314f80b2c3be4ddda9db0f0
ffijuljani1 <ffijuljani1@etf.unsa.ba>
2025-02-14 13:36:07 +0100

    Permission requesting

--- app/src/main/java/com/geocipher/app/MainActivity.kt
+++ app/src/main/java/com/geocipher/app/MainActivity.kt
@@ -9,7 +9,10 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.util.Log
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+
+private const val LOCATION_PERMISSION_REQUEST_CODE = 1001
 
 class MainActivity : AppCompatActivity() {
     private val PERMISSION_ACCESS_FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION
@@ -24,7 +27,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        if(checkLocationPermission()) {
+        if (checkLocationPermission()) {
             getUserLocation()
         } else {
             requestLocationPermission()
@@ -36,11 +39,53 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun requestLocationPermission() {
-        TODO("Not yet implemented")
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        ) {
+            Toast.makeText(
+                this,
+                "You need to provide location access for the app to work.",
+                Toast.LENGTH_LONG
+            ).show()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
     }
 
+
     private fun checkLocationPermission(): Boolean {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        return ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getUserLocation()
+            } else {
+                Toast.makeText(this, "Permission denied. Go to settings  to enable location.", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
 }
