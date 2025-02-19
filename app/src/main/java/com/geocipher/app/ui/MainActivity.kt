499b329a3dbb4d314549fe56e937a6cb5939cea2
ffijuljani1 <ffijuljani1@etf.unsa.ba>
2025-02-19 01:53:21 +0100

    Encryption working after refactoring

--- app/src/main/java/com/geocipher/app/ui/MainActivity.kt
+++ app/src/main/java/com/geocipher/app/ui/MainActivity.kt
@@ -1,42 +1,18 @@
 package com.geocipher.app.ui
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.location.LocationManager
+import com.geocipher.app.implementations.LocationManager
 import android.os.Bundle
-import android.os.Looper
-import android.util.Base64
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.geocipher.app.LOCATION_PERMISSION_REQUEST_CODE
 import com.geocipher.app.R
 import com.geocipher.app.implementations.EncryptionManager
 import com.geocipher.app.implementations.MessageRepository
 import com.geocipher.app.interfaces.LocationUpdateListener
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationRequest
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.Priority
-import com.google.crypto.tink.aead.AeadConfig
-import com.google.firebase.firestore.FirebaseFirestore
-import java.security.SecureRandom
-import java.security.spec.KeySpec
-import java.util.UUID
-import javax.crypto.Cipher
-import javax.crypto.EncryptedPrivateKeyInfo
-import javax.crypto.SecretKeyFactory
-import javax.crypto.spec.GCMParameterSpec
-import javax.crypto.spec.PBEKeySpec
-import javax.crypto.spec.SecretKeySpec
 
 class MainActivity : AppCompatActivity(), LocationUpdateListener {
 
@@ -55,13 +31,24 @@ class MainActivity : AppCompatActivity(), LocationUpdateListener {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        locationManager = LocationManager(this)
+        encryptionManager = EncryptionManager()
+        messageRepository = MessageRepository()
 
 
         textViewLat = findViewById(R.id.latitude)
         textViewLong = findViewById(R.id.longitude)
 
+        val enkriptovana = encryptionManager.encryptMessage(
+            "Fahret",
+            "qwert678"
+        )
 
-        AeadConfig.register()
+        val dekriptovana = encryptionManager.decryptMessage(enkriptovana, "qwert678")
+
+        Log.i("MainActivity", "aaaa")
+        Log.i("MainActivity", "Enkriptovana: $enkriptovana")
+        Log.i("MainActivity", "Dekriptovana: $dekriptovana")
 
     }
 
