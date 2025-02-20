aaff248941d5a1d292fd9c70bc2336020cea0522
ffijuljani1 <ffijuljani1@etf.unsa.ba>
2025-02-20 16:36:30 +0100

    Singletons

--- app/src/main/java/com/geocipher/app/managers/MessageRepositorySingleton.kt
+++ app/src/main/java/com/geocipher/app/managers/MessageRepositorySingleton.kt
@@ -1,4 +1,14 @@
 package com.geocipher.app.managers
 
+import com.geocipher.app.implementations.MessageRepository
+
 object MessageRepositorySingleton {
+    private var instance : MessageRepository? = null
+
+    fun getInstance(): MessageRepository {
+        if(instance == null) {
+            instance = MessageRepository()
+        }
+        return instance!!
+    }
 }
